fc7e4ae8b3cba7ba25b6e8b9c3e5d5e8fe577cb9
Liam Lundberg <liamlundbergnz@gmail.com>
2022-11-19 12:03:35 +1300

    Added item

--- FoodSpace/Models/Item.cs
+++ FoodSpace/Models/Item.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FoodSpace.Models;
 
 public class Item
 {
+	[Key]
 	public int Id  { get; set; }
+	[Required]
 	public string Name { get; set; }
 	public int DisplayOrder { get; set; }
 	public DateTime CreatedDateTime { get; set;} = DateTime.Now;
