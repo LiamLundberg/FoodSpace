2a9a90b3d449d1db008a214b5e69cf9f8e52f9a2
Liam Lundberg <liamlundbergnz@gmail.com>
2022-11-19 12:01:23 +1300

    Add project files.

---
+++ FoodSpace/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodSpace.Models;
+
+public class Item
+{
+	public int Id  { get; set; }
+	public string Name { get; set; }
+	public int DisplayOrder { get; set; }
+	public DateTime CreatedDateTime { get; set;} = DateTime.Now;
+}
+
+
