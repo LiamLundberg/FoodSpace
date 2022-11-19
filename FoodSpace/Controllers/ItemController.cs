9710b7579074811051abf966012ba0b77e081459
Liam Lundberg <liamlundbergnz@gmail.com>
2022-11-19 21:23:17 +1300

    Added Delete Function

--- FoodSpace/Controllers/ItemController.cs
+++ FoodSpace/Controllers/ItemController.cs
@@ -22,8 +22,92 @@ namespace FoodSpace.Controllers
 
         public IActionResult Create()
         {
-            
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order Cannot exactly match name");
+                ModelState.AddModelError("DisplayOrder", "Display Order Cannot exactly match name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Items.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //this can be done as return RedirectToAction("Index", "Home"); if we are going ot antoher controller
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+            return NotFound();
+            }
+            var itemFromDb = _db.Items.Find(id);
+            //var itemFromDbFirst = _db.Items.FirstOrDefault(x => x.Id == id);
+            //var itemFromDBSingle = _db.Items.SingleOrDefault(x => x.Id == id);
+
+            if (itemFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Item obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order Cannot exactly match name");
+                ModelState.AddModelError("DisplayOrder", "Display Order Cannot exactly match name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Items.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //this can be done as return RedirectToAction("Index", "Home"); if we are going ot antoher controller
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var itemFromDb = _db.Items.Find(id);
+            //var itemFromDbFirst = _db.Items.FirstOrDefault(x => x.Id == id);
+            //var itemFromDBSingle = _db.Items.SingleOrDefault(x => x.Id == id);
+
+            if (itemFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Items.Find(id);
+            if(obj == null) { 
+                return NotFound();
+            }
+
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index"); //this can be done as return RedirectToAction("Index", "Home"); if we are going to antoher controller
         }
     }
 }
--- FoodSpace/Models/Item.cs
+++ FoodSpace/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FoodSpace.Models;
 
@@ -8,6 +9,8 @@ public class Item
 	public int Id  { get; set; }
 	[Required]
 	public string Name { get; set; }
+	[DisplayName("Display Order")]
+	[Range(1,100, ErrorMessage = "Please enter a value between 1 and 100!")]
 	public int DisplayOrder { get; set; }
 	public DateTime CreatedDateTime { get; set;} = DateTime.Now;
 }
