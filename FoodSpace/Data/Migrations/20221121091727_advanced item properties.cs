f4226de811f3802a7b5b007054ae09bd0fdbebfb
Liam Lundberg <liamlundbergnz@gmail.com>
2022-11-21 22:42:16 +1300

    cards protein fat

---
+++ FoodSpace/Data/Migrations/20221121091727_advanced item properties.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodSpace.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class advanceditemproperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AllerginNuts",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AllerginSoy",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Carbohydrates",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DietaryFibre",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Energy",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fat",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Protein",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllerginNuts",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "AllerginSoy",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Carbohydrates",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "DietaryFibre",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Energy",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Fat",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Protein",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+        }
+    }
+}
--- FoodSpace/Models/Item.cs
+++ FoodSpace/Models/Item.cs
@@ -6,11 +6,19 @@ namespace FoodSpace.Models;
 public class Item
 {
 	[Key]
-	public int Id  { get; set; }
+	public int Id { get; set; }
 	[Required]
-	public string Name { get; set; }
+	[MaxLength(64, ErrorMessage = "Max Length is 64 Characters!")]
+    public string Name { get; set; }
 	[DisplayName("Display Order")]
 	[Range(1,100, ErrorMessage = "Please enter a value between 1 and 100!")]
 	public int DisplayOrder { get; set; }
+	public bool AllerginNuts { get; set; }
+    public bool AllerginSoy { get; set; }
+    public int Energy { get; set; }
+	public int Protein { get; set; }
+	public int Fat { get; set; }
+	public int Carbohydrates { get; set; }
+	public int DietaryFibre { get; set; }
 	public DateTime CreatedDateTime { get; set;} = DateTime.Now;
 }
